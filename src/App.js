488a1db2ddd1d43f2dcd06e6d29537c571b31e1b
promehedi <admin@promehedi.com>
2022-08-30 03:52:00 +0600

    Fix Signup Issue & Improvements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import ProtectRoute from "./component/ProtectRoute";
-import AccountInfo from "./pages/AccountInfo";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import ProtectRoute from './component/ProtectRoute'
+import AccountInfo from './pages/AccountInfo'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
   return (
-    <>
-      {/* <Nav /> */}
-      <Routes>
-        <Route path="/" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<ProtectRoute />}>
-          <Route path="chat" element={<Home />} />
-          <Route path="profile" element={<AccountInfo />} />
-        </Route>
-      </Routes>
-    </>
-  );
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Signup />} />
+      <Route path='/*' element={<ProtectRoute />}>
+        <Route path='chat' element={<Home />} />
+        <Route path='profile' element={<AccountInfo />} />
+      </Route>
+    </Routes>
+  )
 }
 
-export default App;
+export default App
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,121 +1,124 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Illustration from "../component/Illustration";
-import InputText from "../component/InputText";
-import Lable from "../component/Lable";
-import { useAuth } from "../context/AuthContext";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import Illustration from '../component/Illustration'
+import InputText from '../component/InputText'
+import Lable from '../component/Lable'
+import { useAuth } from '../context/AuthContext'
 
 function Signup() {
-  window.document.title = "Create accout now!";
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  window.document.title = 'Create accout now!'
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
 
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState()
+  const [loading, setLoading] = useState(false)
 
-  const { createAccount, currentUser } = useAuth();
-  const navigate = useNavigate();
-
-  console.log(currentUser);
+  const { createAccount } = useAuth()
+  const navigate = useNavigate()
 
   async function handleSubmit(e) {
-    e.preventDefault();
+    e.preventDefault()
+
+    if (email === '' || password === '' || username === '') {
+      return setError('Please fill out the form')
+    }
+
     if (password !== confirmPassword) {
-      return setError("Password not matched!");
+      return setError('Password not matched!')
     }
 
     try {
-      setLoading(true);
-      setError("");
-      await createAccount(email, password, username);
-      navigate("/chat");
+      setLoading(true)
+      setError('')
+      await createAccount(email, password, username)
+      navigate('/chat')
     } catch (err) {
-      setLoading(false);
-      console.log(err);
-      setError("Faild to create an Account!");
+      setLoading(false)
+      console.log(err)
+      setError('Faild to create an Account!')
     }
   }
 
   return (
-    <div className="container mx-auto px-4 first-letter:">
-      <div className="grid place-items-center grid-cols-1 md:grid-cols-2 h-screen">
-        <Illustration src="./img/signup.jpg" alt="signup" />
-        <form className="w-full" onSubmit={handleSubmit}>
-          <h2 className="text-center uppercase font-bold text-2xl">
+    <div className='container mx-auto px-4 first-letter:'>
+      <div className='grid place-items-center grid-cols-1 md:grid-cols-2 h-screen'>
+        <Illustration src='./img/signup.jpg' alt='signup' />
+        <form className='w-full' onSubmit={handleSubmit}>
+          <h2 className='text-center uppercase font-bold text-2xl'>
             Create an account
           </h2>
           {/* error message */}
           {error && (
-            <p className="bg-red-200 py-3 mt-4 text-center rounded-md">
+            <p className='bg-red-200 py-3 mt-4 text-center rounded-md'>
               {/* Don't create account! try again */}
               {error}
             </p>
           )}
           {/* error message end */}
-          <div className="my-3">
-            <Lable title={"Name"} />
+          <div className='my-3'>
+            <Lable title={'Name'} />
             <InputText
-              type={"text"}
-              placeholder="Name..."
-              id="Name"
+              type={'text'}
+              placeholder='Name...'
+              id='Name'
               required
               value={username}
               onChange={(e) => setUsername(e.target.value)}
             />
           </div>
-          <div className="my-3">
-            <Lable title={"Email"} />
+          <div className='my-3'>
+            <Lable title={'Email'} />
             <InputText
-              type={"email"}
-              placeholder="Email..."
-              id="Email"
+              type={'email'}
+              placeholder='Email...'
+              id='Email'
               required
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
           </div>
-          <div className="my-3">
-            <Lable title={"Password"} />
+          <div className='my-3'>
+            <Lable title={'Password'} />
             <InputText
-              type={"password"}
-              placeholder="Password..."
-              id="Password"
+              type={'password'}
+              placeholder='Password...'
+              id='Password'
               required
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <div className="my-3">
-            <Lable title={"confirm password"} />
+          <div className='my-3'>
+            <Lable title={'confirm password'} />
             <InputText
-              type={"password"}
-              placeholder="Re-type password..."
-              id="confirm_password"
+              type={'password'}
+              placeholder='Re-type password...'
+              id='confirm_password'
               required
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
           </div>
           <button
-            type="submit"
+            type='submit'
             disabled={loading}
-            className="w-full bg-green-400 hover:bg-green-500 transition-all select-none py-2 rounded-md text-xl text-white focus:outline-none"
+            className='w-full bg-green-400 hover:bg-green-500 transition-all select-none py-2 rounded-md text-xl text-white focus:outline-none'
           >
             Sign up
           </button>
 
-          <p className="text-center text-gray-400 mt-5">
+          <p className='text-center text-gray-400 mt-5'>
             already have an account?
-            <Link to="/login" className="text-blue-500">
+            <Link to='/login' className='text-blue-500'>
               login
             </Link>
           </p>
         </form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Signup;
+export default Signup
